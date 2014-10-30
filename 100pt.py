8b2f6562bff3991eccad431d695a921d120b0fdc
Jdsemler <jdsemler@gmail.com>
2014-10-30 15:42:58 -0500

    Lab 12 completed
    70, 85, and 100

--- 100pt.py
+++ 100pt.py
@@ -23,32 +23,40 @@ class MyApp:
 		self.myContainer1.pack()
 		
 		self.button1 = Button(self.myContainer1)
-		self.button1.configure(text="Left", background= "green")
-		self.button1.grid(row=0,column=0)
-		
-	        # Add a second button!
-				
-		
-						
-		# "Bind" an action to the first button												
+ 		self.button1.configure(text="Left", background= "green")
+ 		self.button1.grid(row=0,column=0)
 		self.button1.bind("<Button-1>", self.button1Click)
-		# Create the code to bind an action to the second button
-		# Do not change "<Button-1>"
-		 
-		  
-		# This creates the drawpad - no need to change this 
-		drawpad.pack()
-		
+ 	        # Add a second button!
+						
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="right", background= "green")
+		self.button2.grid(row=0,column=1)		
+		self.button2.bind("<Button-1>", self.button2Click)
+ 						
+ 		# "Bind" an action to the first button												
+		self.button1.bind("<Button-1>", self.button1Click)
+ 		# Create the code to bind an action to the second button
+ 		# Do not change "<Button-1>"
+ 		 
+ 		  
+ 		# This creates the drawpad - no need to change this 
+ 		drawpad.pack()		
 
-		
-	def button1Click(self, event):   
+	def button2Click(self, event):   
 		# Make the oval move to the left!
                 # "global" makes sure that we can access our oval and our drawpad
+		drawpad.move(oval,20,0)
 		global oval
 		global drawpad
-	
-	# Add the event handler for the second button to make it move right!
-	
-		
+			
+ 
+ 		
+ 	def button1Click(self, event):   
+ 		# Make the oval move to the left!
+                 # "global" makes sure that we can access our oval and our drawpad
+		drawpad.move(oval,-20,0)
+ 		global oval
+ 		global drawpad
+
 myapp = MyApp(root)
 root.mainloop()
